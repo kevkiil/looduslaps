3d5a26c3279544120192c63fee1dad5a5498a71c
kevkiil <62466733+kevkiil@users.noreply.github.com>
2022-09-03 20:29:11 +0300

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -22,7 +22,12 @@ const Brand = (props) => {
       >
 
 
-    <text>Looduslaps</text>
+    
+    <text x="50" y="50" font-family="Arial">Arial</text>
+    <text x="100" y="50" font-family="Arial Black">Arial Black</text>
+    <text x="205" y="50" font-family="Helvetica">Helvetica</text>
+
+
 
 
       </svg>
