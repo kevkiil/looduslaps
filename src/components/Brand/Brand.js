c514ef9b427d0e4ddab2effaa97e8a616a849b88
kevkiil <62466733+kevkiil@users.noreply.github.com>
2022-09-03 20:14:11 +0300

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -11,18 +11,13 @@ const Brand = (props) => {
       onClick={() => navigate('/')}
     >
       {/* <h4>SYDNEY</h4> */}
-      <svg
-        width={127}
-        height={24}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 127.67 23.36"
-      >
+      
           
     
     <text x="205" y="50" font-family="Looduslaps">Helvetica</text>
 
 
-      </svg>
+      
     </div>
   );
 };
