d4282cbc05f158cf20b1bfce3e285950d26c59d7
kevkiil <62466733+kevkiil@users.noreply.github.com>
2022-09-03 20:21:58 +0300

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -15,8 +15,7 @@ const Brand = (props) => {
           
     
      <svg
-        width={127}
-        height={24}
+        
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 127.67 23.36"
       >
