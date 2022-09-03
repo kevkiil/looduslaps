60c8394b858510dd05e1432e33e16174fdf3eadd
kevkiil <62466733+kevkiil@users.noreply.github.com>
2022-09-03 20:30:55 +0300

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,30 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
-      
-          
-    
-     <svg
-        width={127}
-        height={23}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 127.67 23.36"
-      >
-
-
-    
-    <text x="50" y="50" font-family="Arial">Arial</text>
-    <text x="100" y="50" font-family="Arial Black">Arial Black</text>
-    <text x="205" y="50" font-family="Helvetica">Helvetica</text>
-
-
-
-
-      </svg>
-
-
-      
+    <h4>Looduslaps</h4>
     </div>
   );
 };
