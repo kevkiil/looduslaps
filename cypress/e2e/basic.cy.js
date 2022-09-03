dd9e626c5a9f701eb1be68b3d645921bb8abf277
kevkiil <62466733+kevkiil@users.noreply.github.com>
2022-09-03 21:03:05 +0300

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,6 +5,6 @@ describe('sample render test', () => {
 
   it('displays the title text', () => {
     cy.get('h2')
-    .contains('2 jumalatest kaksikut soovivad tuua su ellu puhtust ja sygelust jalge vahele');
+    .contains('Puhtad asjad otse loodusest');
   })
 }) 
